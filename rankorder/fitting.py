ef9a57dac1f9265af4f3c401c77ac8ad002a2885
Samuel Haeusler <samuehae@users.noreply.github.com>
2021-08-11 21:17:32 +0200

    implement regression method based on universal rank-order transform

---
+++ rankorder/fitting.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+
+'''general regression method based on universal rank-order transform.
+
+method presented in the following publication: 
+G. Ierley and A. Kostinski. Phys Rev. X 9, 031039
+'''
+
+
+
+import numpy as np
+from rankorder import transform
+
+
+def q_matrix_fit(func, params, xdata, ydata, method='ordinal'):
+    '''calculate Q matrix from residual matrix.
+    
+    residuals are obtained by subtracting model predictions 
+    from each repetition in the data matrix.
+    
+    Parameters
+    ----------
+    func : callable
+        model function func(xdata, *params)
+    params : iterable
+        values for fit parameters
+    xdata : array
+        independent variable with shape (n_s, )
+    ydata : array
+        dependent variable for each repetition and 
+        sampling point with shape (n_r, n_s). 
+        note: missing values are not supported
+    method : {'min', 'max', 'dense', 'ordinal'}
+        ranking method implemented in scipy.stats.rankdata
+    '''
+    
+    # extract shape of data matrix
+    n_r, n_s = ydata.shape
+    
+    # function prediction with shape (n_s, )
+    y = func(xdata, *params)
+    
+    # residuals with shape (n_r, n_s)
+    residuals = ydata - y
+    
+    # calculate Q matrix from residual matrix
+    return transform.data_to_q_matrix(residuals, method)
+
+
+
+def q_rms_fit(func, params, xdata, ydata, method='ordinal'):
+    '''calculate root mean square (rms) of the Q matrix of the residuals.
+    
+    this value measures the error of the regression method and needs to 
+    be minimized by optimizing the parameter values params.
+    
+    Parameters
+    ----------
+    func : callable
+        model function func(xdata, *params)
+    params : iterable
+        values for fit parameters
+    xdata : array
+        independent variable with shape (n_s, )
+    ydata : array
+        dependent variable for each repetition and 
+        sampling point with shape (n_r, n_s). 
+        note: missing values are not supported
+    method : {'min', 'max', 'dense', 'ordinal'}
+        ranking method implemented in scipy.stats.rankdata
+    '''
+    
+    # calculate Q matrix of residuals
+    Q = q_matrix_fit(func, params, xdata, ydata, method)
+    
+    # return root mean square value of elements of matrix Q
+    return np.sqrt(np.mean(Q**2))
